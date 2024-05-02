5aab5ba5092d2a3649d5abb997b366c930babeaf
anstiv <anstivna@gmail.com>
2024-05-02 22:44:14 +0300

    реализована двухуровневая урхитектура тестов

---
+++ test/test_add_contact.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+from fixture.application import Application
+import pytest
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_add_contact(app):
+    app.session.login("admin", "secret")
+    app.contact.create(Contact("Darwin", "Iago", "Ast", "Arctic lemming",
+                               "Dr", "Thoughtmix", "PO Box 20674", "427-565-7932",
+                               "846-844-2236", "269-616-5785", "-", "iast2@wp.com",
+                               "iast2@1und1.de", "iast3@gmail.com", "test", "5",
+                               "October", "1990", "21", "December"))
+    app.session.logout()
+
+
+def test_add_empty_contact(app):
+    app.session.login("admin", "secret")
+    app.contact.create(Contact("", "", "", "", "", "",
+                               "", "", "", "", "", "", "",
+                               "", "", "", "-", "", "", "-"))
+    app.session.logout()
