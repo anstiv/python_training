f6af4ef500d0a4218af82033682d593c26b5b28c
anstiv <anstivna@gmail.com>
2024-05-19 17:34:57 +0300

    реализована проверка валидности фикстуры и проверка состояния сессии

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,8 +2,7 @@ from model.contact import Contact
 
 
 def test_edit_contact(app):
-    app.session.login("admin", "secret")
+
     app.contact.edit_first_contact(Contact("firstname", "middlename", "lastname", "nickname", "", "",
                                            "address", "home", "mobile", "work", "fax", "email", "",
                                            "", "", "1", "December", "1998", "-", "-"))
-    app.session.logout()
--- test/test_edit_group.py
+++
@@ -1,7 +0,0 @@
-from model.group import Group
-
-
-def test_edit_group(app):
-    app.session.login("admin", "secret")
-    app.group.edit_first_group(Group("group_name2", "logo2", "comment2"))
-    app.session.logout()
