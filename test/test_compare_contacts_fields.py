83c6d6e0a608d30c82df17c55b57136c005a29d4
anstiv <anstivna@gmail.com>
2024-06-10 00:18:02 +0300

    Переделан тест для проверки информации о контактах на главной странице

--- test/test_compare_contacts_fields.py
+++ test/test_compare_contacts_fields.py
@@ -1,4 +1,5 @@
 import re
+from model.contact import Contact
 
 
 def test_all_fields_on_home_page(app):
@@ -9,6 +10,19 @@ def test_all_fields_on_home_page(app):
     assert contact_from_home_page.address == contact_from_edit_page.address
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+
+
+def test_compare_fields_on_home_page_and_db(app, db):
+    contact_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    contact_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    for i in range(len(contact_from_db)):
+        assert contact_from_home_page[i].firstname == contact_from_db[i].firstname
+        assert contact_from_home_page[i].lastname == contact_from_db[i].lastname
+        assert contact_from_home_page[i].address == contact_from_db[i].address
+        assert contact_from_home_page[i].all_phones_from_home_page == merge_phones_like_on_home_page(
+            contact_from_db[i])
+        assert contact_from_home_page[i].all_emails_from_home_page == merge_emails_like_on_home_page(
+            contact_from_db[i])
 
 
 def clear(s):
